d28ec6e4910cc68af3af75f7e9e35d8f32b505c8
AusefYousof <ausef.yousof@mail.utoronto.ca>
2025-05-25 19:56:37 -0400

    Implemented heuristic, need to make improvements but we are checking for deadlock and dist of robot to box, and pairwise box/storage point stuff.

---
+++ A1/README.md
@@ -0,0 +1,3 @@
+Development of search algorithms (in solution.py) to solve the Sokoban game within a specified time limit.
+
+Search algorithm's include: Iterative/weighted A*, iterative GBFS, and a custom heuristic that improves on Manhattan distance.
--- A1/solution.py
+++ A1/solution.py
@@ -21,16 +21,84 @@ def heur_alternate(state):
     # Your function should return a numeric value for the estimate of the distance to the goal.
     # EXPLAIN YOUR HEURISTIC IN THE COMMENTS. Please leave this function (and your explanation) at the top of your solution file, to facilitate marking.
 
-    #doing this later havent come up with a good heuristic
-    #possible options: distance from robot to box considered, number of obstacles in direct path
+    """
+    Discussion of alternate heuristic:
+    Manhattan distance suffers from its lack of consideration for the distance a robot has to a given storage box.
+    To illustrate, consider the following simplified configuration, where R denotes robot, S denotes storage, 
+    B denotes box, X denotes obstacle
 
+    XXXXXXXXXXXXXXXXXXXXXXXXXX
+    X                        X
+    X    R           B     S X
+    X   B                  S X
+    X                        X
+    XXXXXXXXXXXXXXXXXXXXXXXXXX
 
-    return heur_manhattan_distance(state)
+    Certainly, it is easy to predict manhattan distance heuristic given that all moves are of equal cost would not have a preference
+    between any moves the R could make in this state, and may end up making its away to the B closer to S for example. This
+    means that we would have to move the robot all the way back to one side of the statespace and push it back due to our misguided heuristic
+    Moving the farther from S box would get us closer to the box we were originally going to move overall (box closer to S), 
+    making for a lower cost solution.
+
+    We can adjust our heuristic to do h = wRB * manhattan_robot_to_box + wBS * manhattan_box_to_storage
+    with wRB and wBS being weights used to give more or less importance to either factor, which will be experimentally determined,
+    although I feel as though robot to box is more important since a move that isnt moving a box is more or less wasted in most cases
+    that I can think of: To simplify lets just consider the x axis/direction consider to box b1 and b2, it is clear that either 
+    b1 or b2 will have an x value less or more than (and therefore closer/farther to storage) its fellow box, meaning the case where 
+    we must move in some direction along x to get a box closer to storage whilst also being able to move said other box is very common.
+
+    Instantly something that comes to mind is the issue of a box getting push into a corner, again consider a simple example with 
+    Manhattan heuristic 
+
+    XXXXXXXXXXXXXXXXXXXXXXXXXX
+    X                        X
+    X    R     B  X        S X
+    X            X           X
+    X                        X
+    XXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    Manhattan distance heuristic which doesnt consider obstacles would not consider the corner B would get trapped in if we were to follow
+    what it believes is most optimal which is to push to the right. Therefore we can implement a function to perform corner/trapped detection
+    and return math.inf as the h value for that state.
+
+    After these two improvements and weight tweaking have only been able to beat benchmark/alternate once on astar
+    so will add improvements.
+
+    Manhattan distance doesnt take into account whether or not a storage will be available to house a given box,
+    my heuristic will adjust the manhattan distance to be ran for each box, and will pair each box with a storage
+    space, such that the total cost is minimized, and will return that cost. This provides a more
+    accurate and realistic depiction of the problem, the following example should illustrate this: 
+
+    XXXXXXXXXXXXXXXXXXXXXXXXXX
+    X  R                     X
+    X    B B               S X
+    X       S                X
+    X                        X
+    XXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    With typical manhattan distance, we would calculate an extremely low cost because of the storage space 
+    in close proximity to both boxes, but that would require that one space to house both boxes, and my 
+    robot to box proximity consideration and deadlock check doesnt do anything to help either,
+    we should calculate the shortest distance from box to storage and remove that from storage so boxes after
+    do not consider a storage spot that will be taken up down the line. 
+    """
+
+    for box in state.boxes:
+        if (deadlock(box, state.obstacles, state.height, state.width)):
+            return math.inf
+
+   #  made improvement on heur_manhattan_distance
+    return _wRB * manhattan_robot_to_box(state.robots, state.boxes) + _wBS * heur_manhattan_distance(state)
+
+    #return _wRB * manhattan_robot_to_box(state.robots, state.boxes) + _wBS * pairwise_manhattan(state.boxes, state.storage)
+
 
 # CONSTANTS
-
 #factor to multiply (decrease) heuristic weight for iterative astar
-_WEIGHT_FACTOR = 0.7
+_WEIGHT_FACTOR = 0.5
+#Custom heuristic weights, wRB = weight robot to box, wBS = weight box to storage
+_wRB = 1
+_wBS = 0.5
 
 
 def heur_zero(state):
@@ -52,7 +120,7 @@ def heur_manhattan_distance(state):
     total_dist = 0
 
     for box in state.boxes:
-        total_dist += find_closest(box, state.storage)[0]
+        total_dist += closest_box_to_storage(box, state.storage)[0]
 
     return total_dist
 
@@ -77,6 +145,8 @@ def weighted_astar(initial_state, heur_fn, weight, timebound):
     '''OUTPUT: A goal state (if a goal is found), else False as well as a SearchStats object'''
     '''implementation of weighted astar algorithm'''
 
+    timebound -= 0.015
+
     SE, setup_time = init_weighted_astar_SE(initial_state, heur_fn, weight, cc_level="full")
 
     return SE.search(timebound=timebound - setup_time)
@@ -87,6 +157,10 @@ def iterative_astar(initial_state, heur_fn, weight=1, timebound=5):  # uses f(n)
     '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
     '''OUTPUT: A goal state (if a goal is found), else False as well as a SearchStats object'''
     '''implementation of iterative astar algorithm'''
+
+    #initial autograder test prints time search is taking, tend to take ~0.01 longer than timebound,
+    #subtract 0.015 from given timebound so search accounts for this
+    timebound -= 0.015
 
     goal_state, better_goal_state = None, None
 
@@ -111,7 +185,6 @@ def iterative_astar(initial_state, heur_fn, weight=1, timebound=5):  # uses f(n)
     if goal_state:
         costbound[2] = goal_state.gval
     else:
-        print("Goal state not found in initial search for iterative astar")
         return goal_state, stats
 
     while not time_up:
@@ -145,6 +218,8 @@ def iterative_gbfs(initial_state, heur_fn, timebound=5):  # only use h(n)
     '''OUTPUT: A goal state (if a goal is found), else False'''
     '''implementation of iterative gbfs algorithm'''
 
+    timebound -= 0.015
+
     #Follows general structure of iterative a star, refer to iterative_astar() for comments
 
     goal_state, better_goal_state = None, None
@@ -164,7 +239,6 @@ def iterative_gbfs(initial_state, heur_fn, timebound=5):  # only use h(n)
     if goal_state:
         costbound[0] = goal_state.gval
     else:
-        print("Goal state not found in initial search for iterative astar")
         return goal_state, stats
 
     while not time_up:
@@ -184,7 +258,7 @@ def iterative_gbfs(initial_state, heur_fn, timebound=5):  # only use h(n)
         return goal_state, stats
 
 #HELPERS
-def find_closest(box, storage):
+def closest_box_to_storage(box, storage):
     """
     Find closest storage point to a given box (in terms of manhattan distance)
 
@@ -207,6 +281,96 @@ def find_closest(box, storage):
             closest_point = storage_point
     
     return min, closest_point
+
+def manhattan_robot_to_box(robots, boxes):
+    """
+    Find sum of all closest distances of each robot to a box
+
+    @param set robots: robots positions
+    @param set boxes: boxes positions
+    @rtype: total: total distance
+    """
+
+    total = 0
+
+    for robot in robots:
+        min = math.inf
+        for box in boxes:
+            distance = abs(box[0] - robot[0]) + abs(box[1] - robot[1])
+            if distance < min:
+                min = distance
+        total += min
+    
+    return total
+
+def deadlock(box, obstacles, height, width):
+    """
+    Check if box in deadlock (two adjacent corners have obstacles)
+    returns infinity as h value
+
+    @param box: box to check for
+    @param obstacles: set of obstacles on board
+    @rtype: bool -> True if box in deadlock, False if not
+    """
+
+    #corner of board checking
+    if ((box[0] - 1 <= 0 and box[1] - 1 <= 0)
+        or (box[0] - 1 <= 0 and box[1] + 1 >= height)
+        or (box[0] + 1 >= width and box[1] - 1 <= 0)
+        or (box[0] + 1 >= width and box[1] + 1 >= height)):
+        return True
+
+    possible_deadlocks = [[(box[0] + 1, box[1]), (box[0], box[1] + 1)], #right and down blocked
+                          [(box[0] + 1, box[1]), (box[0], box[1] - 1)], #right and up blocked
+                          [(box[0] - 1, box[1]), (box[0], box[1] - 1)], #left and up blocked
+                          [(box[0] - 1, box[1]), (box[0], box[1] + 1)]] #left and down blocked
+    
+    for deadlock_pair in possible_deadlocks:
+        for obst1, obst2 in [deadlock_pair]:
+            if obst1 in obstacles and obst2 in obstacles:
+                return True
+
+    return False
+
+def pairwise_manhattan(boxes, storage):
+    """
+    Pair a box with its closest storage point, remove that point for other boxes
+
+    @param boxes: boxes avail
+    @param storage: set of storage points on the board
+    @rtype: int -> total cost for getting each box to storage point by manhattan
+    """
+    storage_c = list(storage)
+
+    #[b1, b2, b3]
+    #[s1, s2, s3]
+
+    total_cost = 0
+
+    for box in boxes:
+        if box in storage_c:
+            storage_c.remove(box)
+            continue
+
+        closest = math.inf
+        index_of_closest = -1
+        for j in range(len(storage_c)):
+            dist = manhattan_helper(box, storage_c[j])
+            
+            if dist < closest:
+                closest = dist
+                index_of_closest = j
+                
+        total_cost += closest
+        del storage_c[index_of_closest]
+
+    return total_cost
+
+def manhattan_helper(p1, p2):
+    #small helper to get manhattan of two points used in pairwise_manhattan
+    return abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])
+
+#Sub-section of helpers, search engine init/modification functions
 
 def init_iterative_gbfs_SE(initial_state, heur_fn, timebound, cc_level="full"):
     """
@@ -271,6 +435,21 @@ def adjust_SE(SE, initial_state, heur_fn, new_weight):
 
     SE.init_search(initial_state, goal_fn=sokoban_goal_state, 
                    heur_fn=heur_fn, fval_function=wrapped_fval_function)
+    
+
+if __name__=='__main__':
+    ss = SokobanState("START", 0, None, 10, 10,  # dimensions
+                 ((2, 2),),  # robots
+                 frozenset(((3, 3), (0, 0), (1, 0))),  # boxes
+                 frozenset(((0, 1), (0, 2), (0, 4))),  # storage
+                 frozenset(((2, 3), (3, 2)))  # obstacles
+                 )
+    
+
+
+    goal_state, stats = iterative_gbfs(ss, heur_alternate, timebound = 10)
+
+
 
 
 
